6d99e645367b7985bfb9dd3a4b1d82b3ca897bf5
Shashank Gopikrishna <shashank.gopikrishna@gmail.com>
2019-07-07 01:45:09 -0400

    testing log to screen

--- cherry_flask/core.py
+++ cherry_flask/core.py
@@ -9,6 +9,15 @@ class CherryFlask(object):
 	def __init__(self, app, scheduler=None):
 		self.app = app
 		self.sched = scheduler
+		@app.after_request
+		def teardown(response): # pylint: disable=unused-variable
+			print(cherrypy.serving.response)
+			# if app.config['LOG']:
+			# 	adr = request.environ.get('HTTP_X_REAL_IP', request.environ.get('REMOTE_ADDR'))
+			# 	mth = request.environ.get('REQUEST_METHOD')
+			# 	pth = request.environ.get('PATH_INFO')
+			# 	print(f'''{adr} - [{dt.now().strftime('%d/%b/%Y %H:%M:%S')}] - "{mth} {pth}" - {response.status_code}''')
+			return response
 
 	def run(self, host='0.0.0.0', port=8080, threads=5, debug=False):
 		if not debug: cherrypy.config.update({'engine.autoreload.on' : False})
