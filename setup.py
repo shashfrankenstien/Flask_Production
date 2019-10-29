65f7e4921d98f7d2004dd06f9305b787594b5159
Shashank Gopikrishna <shashank.gopikrishna@gmail.com>
2019-10-28 23:39:34 -0400

    Added License

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='flask_production',
-	version='1.0.0',
+	version='1.0.1',
 	author='Shashank Gopikrishna',
 	author_email='shashank.gopikrishna@gmail.com',
 	packages=['flask_production'],
