a058e772d0cf4b225a165f4c3ee4055f0457194d
Shashank Gopikrishna <shashank.gopikrishna@gmail.com>
2020-07-06 09:04:17 -0400

    format time taken for job; chrome scrollbar fix

--- flask_production/plugins.py
+++ flask_production/plugins.py
@@ -46,7 +46,7 @@ def TD(content, css=[], colspan=1, rowspan=1):
 		' '.join(css),
 		colspan,
 		rowspan,
-		content
+		content if content is not None else "-"
 	)
 
 def TR(row):
@@ -68,6 +68,25 @@ class ReadOnlyTaskMonitor(object):
 				flex-direction:column;
 				align-items:center;
 			}
+			html {
+				--console-bg: #3d3d3d;
+				--thumb-bg: grey;
+			}
+			*::-webkit-scrollbar {
+				width: 11px;
+			}
+			* {
+				scrollbar-width: thin;
+				scrollbar-color: var(--thumb-bg) var(--console-bg);
+			}
+			*::-webkit-scrollbar-track {
+				background: var(--console-bg);
+			}
+			*::-webkit-scrollbar-thumb {
+				background-color: var(--thumb-bg) ;
+				border-radius: 6px;
+				border: 3px solid var(--console-bg);
+			}
 			h2 { padding-top:10px;}
 			table {
 				border-spacing: 5px;
@@ -82,7 +101,7 @@ class ReadOnlyTaskMonitor(object):
 			}
 			th {
 				height:2vh;
-				background-color:#3d3d3d;
+				background-color:var(--console-bg);
 				color:white;
 			}
 			td.grey {color:#c2c2c2;}
@@ -152,13 +171,12 @@ class ReadOnlyTaskMonitor(object):
 				overflow:hidden;
 			}
 			.console {
-				background-color:#3d3d3d;
+				background-color:var(--console-bg);
 				color:white;
 			}
 			.log_table td.console > div {
 				width:100%;
 				height:92vh;
-				scrollbar-color: grey #3d3d3d;
 				overflow-anchor: none;
 				overflow:scroll;
 				white-space: nowrap;
@@ -189,6 +207,9 @@ class ReadOnlyTaskMonitor(object):
 	def __html_wrap(self, *args):
 		return HTML(''.join(args), title="{} Task Monitor".format(self._display_name))
 
+	def __htmlify_text(self, t):
+		return str(t).strip().replace("\n", "<br>").replace("\t", "&#9;").replace(' ', '&nbsp;')
+
 	def __job_state(self, jdict):
 		state = 'READY'
 		if jdict['is_running']:
@@ -214,26 +235,22 @@ class ReadOnlyTaskMonitor(object):
 	def __job_duration(self, jdict):
 		duration = None
 		if jdict['logs']['start'] is not None and jdict['logs']['end'] is not None:
-			duration = jdict['logs']['end']-jdict['logs']['start']
+			seconds = (jdict['logs']['end']-jdict['logs']['start']).seconds
+			if seconds >= 60:
+				minutes = seconds // 60
+				seconds = seconds % 60
+				duration = "{}:{} minutes".format(minutes, seconds)
+			elif seconds==1:
+				duration = "{} second".format(seconds)
+			else:
+				duration = "{} seconds".format(seconds)
 		return duration
 
 	def __job_schedule_str(self, jdict):
 		return "every {} seconds".format(jdict['every']) if isinstance(jdict['every'], int) else "every {} at {}".format(jdict['every'], jdict['at'])
 
 	def __job_dictlist_to_html(self, d):
-		rows = []
-		for row in d:
-			tds = []
-			for cell in row.values():
-				if isinstance(cell, dict):
-					cell = self.__job_dictlist_to_html([cell])
-				if cell is None:
-					cell = '-'
-
-				cell_str = str(cell).replace("\n", "<br>")
-				tds.append(TD(cell_str, self.__job_state_css(cell_str)))
-			rows.append(TR(tds))
-		return TABLE(thead=THEAD(d[0].keys()), tbody=TBODY(rows))
+		return
 
 	def __show_all(self):
 		d = []
@@ -242,15 +259,17 @@ class ReadOnlyTaskMonitor(object):
 			duration = self.__job_duration(jd)
 			state = self.__job_state(jd)
 			d.append({
-				'Id': i,
-				'Name': jd['func'],
-				'Schedule': self.__job_schedule_str(jd),
-				'Description': jd['doc'].strip().split('\n')[0] + "..." if jd['doc'] is not None else '-',
-				'State': state,
-				'Time Taken': duration,
-				'Next Run': jd['next_run'],
-				'More':"<a href='/{}/{}'><button>show more</button><a>".format(self._endpoint, i)
+				'Id': TD(i),
+				'Name': TD(jd['func']),
+				'Schedule': TD(self.__job_schedule_str(jd)),
+				'Description': TD(jd['doc'].strip()[:30] + "..." if jd['doc'] is not None else "-"),
+				'State': TD(state, css=self.__job_state_css(state)),
+				'Time Taken': TD(duration),
+				'Next Run': TD(jd['next_run']),
+				'More':TD("<a href='/{}/{}'><button>show more</button><a>".format(self._endpoint, i))
 			})
+		rows = [TR(row.values()) for row in d]
+		all_jobs_table = TABLE(thead=THEAD(d[0].keys()), tbody=TBODY(rows))
 
 		auto_reload_script = '''
 		window.addEventListener('load', (event) => {{
@@ -260,34 +279,32 @@ class ReadOnlyTaskMonitor(object):
 		return self.__html_wrap(
 			self.STYLES,
 			H(2, "{} - Task Monitor".format(self._display_name)),
-			self.__job_dictlist_to_html(d),
+			all_jobs_table,
 			SCRIPT(auto_reload_script)
 		)
 
 	def __show_one(self, n):
 		jobd = self.sched.jobs[n].to_dict()
-		htmlify_text = lambda t: str(t).strip().replace("\n", "<br>").replace("\t", "&#9;").replace(' ', '&nbsp;')
 		titleTD = lambda t: TD(t, 'title')
-		modTD = lambda t,css=[]: TD("-" if t is None else t, css)
 		state = self.__job_state(jobd)
+
 		rows = [
 			TR([ titleTD("Schedule"), TD(self.__job_schedule_str(jobd)), ]),
-			TR([ titleTD("State"), modTD(state, self.__job_state_css(state)) ]),
-			TR([ titleTD("Time Taken"), modTD(self.__job_duration(jobd)) ]),
+			TR([ titleTD("State"), TD(state, self.__job_state_css(state)) ]),
+			TR([ titleTD("Time Taken"), TD(self.__job_duration(jobd)) ]),
 			TR([ titleTD("Next Run In"), "<td id='next-run-in'>-<td>" ]),
 		]
 		info_table = TABLE(tbody=TBODY(rows), css='info_table')
-		description_div = DIV(htmlify_text(jobd['doc'])) if jobd['doc'] is not None else ''
+		description_div = DIV(self.__htmlify_text(jobd['doc'])) if jobd['doc'] is not None else ''
 		monitor_div = DIV(
 			H(2, jobd['func']) + info_table + description_div,
 			css="monitor"
 		)
 
 		logs_row = TR([
-			TD( DIV( htmlify_text(jobd['logs']['log']) + DIV('', css='anchor-div') ), css="console"),
-			TD( DIV( htmlify_text(jobd['logs']['err']) + DIV('', css='anchor-div') ), css="console"),
+			TD( DIV( self.__htmlify_text(jobd['logs']['log']) + DIV('', css='anchor-div') ), css="console"),
+			TD( DIV( self.__htmlify_text(jobd['logs']['err']) + DIV('', css='anchor-div') ), css="console"),
 		])
-
 		logs_table = TABLE(thead=THEAD(['Logs', 'Traceback']), tbody=TBODY(logs_row), css='log_table')
 		logs_div = DIV( logs_table, css="logs_div" )
 
@@ -295,7 +312,6 @@ class ReadOnlyTaskMonitor(object):
 			monitor_div + logs_div,
 			css="container"
 		)
-
 		auto_reload_script = '''
 		let running = {}
 		let next_run = Date.parse("{}")
