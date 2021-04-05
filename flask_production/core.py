dc13c64fa07a3c8e99ba7f7beaa77c8d5a694352
Shashank Gopikrishna <shashank.gopikrishna@gmail.com>
2021-04-05 10:15:17 -0400

    add flag to disable CherryFlask after_request print

--- flask_production/core.py
+++ flask_production/core.py
@@ -7,16 +7,19 @@ from datetime import datetime as dt
 
 class CherryFlask(object):
 
-	def __init__(self, app, scheduler=None):
+	__slots__ = ['app', 'sched']
+
+	def __init__(self, app, scheduler=None, silent=False):
 		self.app = app
 		self.sched = scheduler
-		@app.after_request
-		def teardown(response): # pylint: disable=unused-variable
-			adr = request.environ.get('HTTP_X_REAL_IP', request.environ.get('REMOTE_ADDR'))
-			mth = request.environ.get('REQUEST_METHOD')
-			pth = request.environ.get('PATH_INFO')
-			print(f'''{adr} - [{dt.now().strftime('%m/%d/%Y %H:%M:%S')}] - "{mth} {pth}" - {response.status_code}''')
-			return response
+		if not silent:
+			@app.after_request
+			def teardown(response): # pylint: disable=unused-variable
+				adr = request.environ.get('HTTP_X_REAL_IP', request.environ.get('REMOTE_ADDR'))
+				mth = request.environ.get('REQUEST_METHOD')
+				pth = request.environ.get('PATH_INFO')
+				print(f'''{adr} - [{dt.now().strftime('%m/%d/%Y %H:%M:%S')}] - "{mth} {pth}" - {response.status_code}''')
+				return response
 
 	def run(self, host='0.0.0.0', port=8080, threads=5, debug=False):
 		if not debug: cherrypy.config.update({'engine.autoreload.on' : False})
